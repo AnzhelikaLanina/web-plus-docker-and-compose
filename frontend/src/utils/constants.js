5ae923b0f36677ec8c8c00a72fbc425d68756974
AnzhelikaLanina <anzhelika.sidovi4eva@yandex.ru>
2024-08-08 00:45:09 +0300

    исправлен адрес

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "api.kupipodariday.project.nomorepartiesco.ru";
+export const URL = "https://api.kupipodariday.project.nomorepartiesco.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
